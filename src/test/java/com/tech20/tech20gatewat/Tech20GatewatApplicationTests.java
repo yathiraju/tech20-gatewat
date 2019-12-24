b25dcb4c6c6a66c1debb5c507ca08948ec7827d3
yathi <kolli.yathi@gmail.com>
2019-12-24 00:28:45 -0500

    Initial commit

---
+++ src/test/java/com/tech20/tech20gatewat/Tech20GatewatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech20.tech20gatewat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech20GatewatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
